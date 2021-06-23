00a1b553fc8011a4d0fbb60be84d677c6326220c
Alpha-geek01 <Umeshsheker01@gmail.com>
2021-06-23 23:50:13 +0530

    add localstorage

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const taskContainer = document.querySelector(".task__container")
 
+const globalStore = [];
+
 const generateNewCard = (taskData) => `
 <div class="col-md-6 col-lg-4" id=${taskData.id}>
 <div class="card">
@@ -32,6 +34,26 @@ const generateNewCard = (taskData) => `
 </div>
 `;
 
+const loadInitialCardData = () => {
+  //localStorage to get Tasky card data
+
+  const getCardData = localStorage.getItem("tasky");
+
+  // convert to normal object
+
+  const {cards} = JSON.parse(getCardData);
+
+  // loop over those array of task object to create HTML card, inject it to DOM
+
+  cards.map((cardObject) => {
+    taskContainer.insertAdjacentHTML("beforeend", generateNewCard(cardObject));
+
+    // update our globalStore
+    globalStore.push(cardObject);
+  }) 
+
+};
+
 const saveChanges = () => {
     const taskData ={ 
         id: `${Date.now()}`, //Unique number for id
@@ -42,4 +64,8 @@ const saveChanges = () => {
     };
 
     taskContainer.insertAdjacentHTML("beforeend", generateNewCard(taskData));
+
+    globalStore.push(taskData);
+
+    localStorage.setItem("tasky",JSON.stringify({cards:globalStore}));
 };
